f83614477b5e42413ea5ebd85a0fd4035d034dc4
emanuelaristimuno <emanueluader@gmail.com>
2021-12-13 13:02:49 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-nukrxq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-nukrxq)
---
+++ script.js
@@ -0,0 +1,4 @@
+function block_none() {
+  document.getElementById('hidden-div').classList.add('show');
+  document.getElementById('button-id').classList.add('hide');
+}
